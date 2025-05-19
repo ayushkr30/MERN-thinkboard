9fff0fda66f4a0f2b4fc3531417c1a62c018969a
Ayush Kumar <ayushpal2017ak@gmail.com>
2025-05-19 23:46:23 +0530

    frontend started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Routes } from 'react-router';
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        
+      </Routes>
+    </div>
+  )
+}
+
+export default App;
