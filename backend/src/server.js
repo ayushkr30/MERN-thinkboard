7f4afdb7cf5159ecdefcadcc013f8bf8724e912c
Ayush Kumar <ayushpal2017ak@gmail.com>
2025-05-20 02:22:32 +0530

    completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/rateLimiter.js";
-
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -12,8 +12,10 @@ const PORT = process.env.PORT || 5001;
 
 
 //middleware
+app.use(cors());
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
+
 
 
 app.use("/api/notes",  notesRoutes);
